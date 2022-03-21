c0b6e48275e3ac18058441bdb907cf458046f921
Garridot <garridot210@gmail.com>
2022-03-21 12:00:07 -0300

    update update_orderdetails

--- clicoh_api/ecommerce/utils.py
+++ clicoh_api/ecommerce/utils.py
@@ -1,7 +1,5 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import serializers
-
 from database.models import Product,OrderDetail
 
 
@@ -11,7 +9,6 @@ class OrderTools:
     def return_stock(instance):
 
         # Se retornara la cantidad solicitada al stock disponible de lo productos
-
         products = OrderDetail.objects.filter(order=instance)        
 
         for i in products:
@@ -26,7 +23,6 @@ class OrderDetailsTools:
     def stock_control(data):
 
         # Verifica si hay stock disponible.
-
         product  = Product.objects.get(id=data['product'])        
         stock    = product.stock 
         cuantity = int(data['cuantity'])    
@@ -46,19 +42,23 @@ class OrderDetailsTools:
 
 
     def update_orderdetails(instance,data): 
-       
-        # Se retornara la cantidad solicitada al stock disponible del producto. 
-        # Una vez hecho esto, se enviara la nueva cantidad solicitada.          
 
+        n_product  = data['product']
+        n_cuantity = data['cuantity']
+       
+        # Se retornara la cantidad solicitada al stock disponible del producto
+        
         OrderDetailsTools.return_stock(instance) 
 
+        # Una vez hecho esto, se enviara la nueva cantidad solicitada.
+
         # Verifica si se solicito el mismo producto.
-        if int(instance.product.id) != int(data['product']):              
+        if int(instance.product.id) != int(n_product):              
             
-            return OrderDetailsTools.stock_control({'product':data['product'],'cuantity':data['cuantity']})  
+            return OrderDetailsTools.stock_control({'product':n_product,'cuantity':n_cuantity})  
         
         else:        
-            return OrderDetailsTools.stock_control({'product':instance.product.id,'cuantity':data['cuantity']})         
+            return OrderDetailsTools.stock_control({'product':instance.product.id,'cuantity':n_cuantity})         
 
 
     def return_stock(instance):
