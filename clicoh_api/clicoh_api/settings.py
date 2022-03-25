964fef84dbd20f26918a1bd6ba1d2e07eef9f84f
Tomas Garrido <81374799+Garridot@users.noreply.github.com>
2022-03-25 16:04:21 -0300

    Update settings.py

--- clicoh_api/clicoh_api/settings.py
+++ clicoh_api/clicoh_api/settings.py
@@ -94,12 +94,6 @@ WSGI_APPLICATION = 'clicoh_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     'default': {
